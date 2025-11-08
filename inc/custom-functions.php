cde0dcc58b973ac5c365730553481342686257e0
Ferdous Ahmed <ferdous935174@gmail.com>
2025-11-08 21:38:30 +0600

    Revert "add condition for 3 date"
    This reverts commit c952d4e8c1e2c47871acbf1a72dd8bf850466545.

--- inc/custom-functions.php
+++ inc/custom-functions.php
@@ -1042,16 +1042,12 @@ if (! function_exists('create_json_object_by_product_id')) {
                 $formatted_item['time'] = sanitize_text_field($course['la_phleb_course_time']);
             }
 
-            // Add dummy item 2 and 3 days before each date, fully booked
-            if (count(explode(',', $course_date)) == 3) {
-                $dummy_date = date('d F Y', strtotime($course_date) - 2 * 24 * 3600);
-                $dummy_item = $formatted_item;
-                $dummy_item['quota'] = 1;
-                $dummy_item['seat'] = 0;
-                $dummy_item['real'] = 0;
-                $dummy_item['date'] = $dummy_date;
-                $formatted_items[] = $dummy_item;
-            }
+            // Add dummy item 2 days before each date, fully booked
+            $dummy_date = date('d F Y', strtotime($course_date) - 2 * 24 * 3600);
+            $dummy_item = $formatted_item;
+            $dummy_item['quota'] = 1;
+            $dummy_item['date'] = $dummy_date;
+            $formatted_items[] = $dummy_item;
 
             if ($delete_flag != 1) {
                 $formatted_items[] = $formatted_item;
