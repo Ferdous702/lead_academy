cd6c4bcc4fe8a2f96e7f9c7d1d3847171f83a0b2
Ferdous Ahmed <ferdous935174@gmail.com>
2025-11-12 15:16:19 +0600

    again 2

--- inc/custom-functions.php
+++ inc/custom-functions.php
@@ -1040,7 +1040,8 @@ if (! function_exists('create_json_object_by_product_id')) {
         if (is_array($course_meta_group) && !empty($course_meta_group)) {
             $existing_dates = array_map(function ($course_item) {
                 $date_to_check = $course_item['adv_course_date'] ?? $course_item['la_phleb_course_date'];
-                return date('d F Y', strtotime($date_to_check));
+                $formatted_date = date('d F Y', strtotime($date_to_check));
+                return get_date_with_ordinal_suffix($formatted_date);
             }, $course_meta_group);
             $existing_dates = array_unique($existing_dates);
         }
